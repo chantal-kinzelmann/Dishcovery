22931370c2120afce049d6a174537327ef02ffaf
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-03-04 13:26:30 +0100

    updated searchbar with angular material (need to fix styling)

--- Dishcovery/src/app/searchbar/searchbar.component.ts
+++ Dishcovery/src/app/searchbar/searchbar.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-searchbar',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.scss'
 })
 export class SearchbarComponent {
+  // @Input('recipes') recipes$!: Observable<Recipe []>;
+  // recipes$?: Observable<string[]>;
+  // searchControl = new FormControl('');
+  // @Output('SelectedRecipe') selectedRecipe$: Observable<string> = this.searchControl.valueChanges.pipe(map(value => value || ''));
 
+  // ngOnInit(): void {
+  //   this.recipes$ = 
+  //   );
+  // }
 }
