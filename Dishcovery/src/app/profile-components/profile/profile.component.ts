dfbb3b7fd3d9f1c636f902d96fbdd3c458acfeec
CHANTAL KINZELMANN <chantal.kinzelmann@mercedes-benz.com>
2025-03-04 20:22:50 +0100

    routing geht wieder nicht :(

--- Dishcovery/src/app/profile-components/profile/profile.component.ts
+++ Dishcovery/src/app/profile-components/profile/profile.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MyFavoritesComponent } from "../my-favorites/my-favorites.component";
 import { MyRecipesComponent } from "../my-recipes/my-recipes.component";
 import {MyWatchListComponent } from '../my-watch-list/my-watch-list.component';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-profile',
   standalone: true,
