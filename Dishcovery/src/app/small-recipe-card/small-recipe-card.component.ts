bbfce9bc85aa9abf8d6d0a915c2484440aba8dbc
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-03-05 13:27:28 +0100

    Updated website title

--- Dishcovery/src/app/small-recipe-card/small-recipe-card.component.ts
+++ Dishcovery/src/app/small-recipe-card/small-recipe-card.component.ts
@@ -2,18 +2,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-small-recipe-card',
   imports: [
     MatCardModule,
     MatButtonModule,
-    MatChipsModule
+    MatChipsModule,
+    MatIconModule
   ],
   templateUrl: './small-recipe-card.component.html',
   styleUrl: './small-recipe-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SmallRecipeCardComponent {
-
+  rating: number = 4;
 }
