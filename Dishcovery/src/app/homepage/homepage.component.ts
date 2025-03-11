04b26e9bcde4fda726da9733b5bcad4f1f01c3a8
CHANTAL KINZELMANN <chantal.kinzelmann@mercedes-benz.com>
2025-03-11 16:27:47 +0100

    vorarbeite für Homepage

--- Dishcovery/src/app/homepage/homepage.component.ts
+++ Dishcovery/src/app/homepage/homepage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HomepageNavBarComponent } from './homepage-nav-bar/homepage-nav-bar.component';
+import { SmallRecipeCardComponent } from '../small-recipe-card/small-recipe-card.component';
 @Component({
   selector: 'app-homepage',
-  imports: [HomepageNavBarComponent],
+  imports: [HomepageNavBarComponent,SmallRecipeCardComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
