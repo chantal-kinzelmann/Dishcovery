89e079ccaa2dbd6ae8a499e04513514af2a563b4
CHANTAL KINZELMANN <chantal.kinzelmann@mercedes-benz.com>
2025-03-11 15:56:21 +0100

    Darkmode switch hinzugefügt

--- Dishcovery/src/app/header/header.component.ts
+++ Dishcovery/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { LoginButtonComponent } from '../homepage/login-button/login-button.component';
 import { SearchbarComponent } from '../homepage/searchbar/searchbar.component';
 import { RouterLink } from '@angular/router';
@@ -10,5 +10,32 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isDarkMode = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    // Prüfe, ob Darkmode gespeichert ist
+    const darkModeSetting = localStorage.getItem('darkMode');
+    this.isDarkMode = darkModeSetting === 'enabled';
+
+    this.updateDarkMode();
 }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode ? 'enabled' : 'disabled');
+    this.updateDarkMode();
+  }
+
+  updateDarkMode() {
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-mode');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-mode');
+    }
+  }
+}
+
+
+
