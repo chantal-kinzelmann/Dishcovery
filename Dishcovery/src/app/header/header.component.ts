4f38bb62518fca5cc3c9863d2e92cf6d971d4a0f
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-03-12 10:09:30 +0100

    Highlight home button when header routing to homepage

--- Dishcovery/src/app/header/header.component.ts
+++ Dishcovery/src/app/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginButtonComponent } from '../homepage/login-button/login-button.component';
 import { SearchbarComponent } from '../homepage/searchbar/searchbar.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [LoginButtonComponent, SearchbarComponent, RouterLink],
+  imports: [LoginButtonComponent, SearchbarComponent, RouterLink, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
