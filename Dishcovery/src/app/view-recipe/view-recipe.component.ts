bf852a03cf537ab7b86abcf3526d158c0b1041ec
Ju <julian@kaiserv.de>
2025-03-15 12:26:04 +0100

    Added Profile-Pic + comment form

--- Dishcovery/src/app/view-recipe/view-recipe.component.ts
+++ Dishcovery/src/app/view-recipe/view-recipe.component.ts
@@ -1,16 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-view-recipe',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './view-recipe.component.html',
   styleUrl: './view-recipe.component.scss'
 })
 export class ViewRecipeComponent {
   public portions: number = 1;
   public currentRating: number = 2;
-  constructor() { }
+  userRating:number = 0;
+  newComment:string="";
+
+  setRating(star:number){
+    this.userRating=star;
+  }
+
+  onSubmit(form: NgForm){
+    const commentText = form.value.commentText;
+    const rating = form.value.rating;
+    console.log(form);
+    console.log(commentText);
+    console.log(rating);
+    form.reset();
+    this.userRating=0;
+  }
 
   updateIngredients():void{
     
