126613c0318654557ce3eb3875aacacb5212f841
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-03-25 12:05:41 +0100

    filtering by tags works properly !! :) ! :D

--- Dishcovery/src/app/recipes-page/recipes-page.component.ts
+++ Dishcovery/src/app/recipes-page/recipes-page.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe, Tag } from '../services/recipe-services/recipe.type';
-import { map, Observable, tap } from 'rxjs';
+import { Observable, BehaviorSubject, combineLatest, map } from 'rxjs';
 import { RecipeService } from '../services/recipe-services/recipe.service';
 import { SmallRecipeCardComponent } from '../small-recipe-card/small-recipe-card.component';
 import { CommonModule } from '@angular/common';
@@ -12,59 +12,70 @@ import { MatChipsModule } from '@angular/material/chips';
   templateUrl: './recipes-page.component.html',
   styleUrl: './recipes-page.component.scss'
 })
-export class RecipesPageComponent {
-  recipes$: Observable<Recipe[]>;
+export class RecipesPageComponent implements OnInit {
+  // Original recipes observable
+  private recipes$: Observable<Recipe[]>;
+  
+  // Subject to track selected tags
+  private selectedTagsSubject = new BehaviorSubject<string[]>([]);
+  
+  // Observable for selected tags
+  selectedTags$ = this.selectedTagsSubject.asObservable();
+  
+  // Observable for all tags
   allTags$?: Observable<string[]>;
-  recipesByTag$?: Observable<{ [tag: string]: Recipe[] }>;
+  
+  // Observable for filtered recipes
+  filteredRecipes$?: Observable<Recipe[]>;
 
   constructor(private readonly recipeService: RecipeService) {
-    this.recipes$ = this.recipeService.getAllRecipes().pipe(
-      tap(recipes => console.log('Received recipes:', recipes))
-    );
+    this.recipes$ = this.recipeService.getAllRecipes();
   }
 
   ngOnInit() {
-    // Create an observable that groups recipes by their tags
-    this.recipesByTag$ = this.recipes$.pipe(
-      tap(recipes => {
-        console.log('All Recipes:', recipes);
-        console.log('Number of Recipes:', recipes.length);
-      }),
-      map(recipes => {
-        // Reduce recipes into an object where keys are tag names and values are arrays of recipes
-        const tagMap = recipes.reduce((acc, recipe) => {
-          console.log('Current Recipe:', recipe);
-          // Flatten tags and ensure unique tag names
-          const recipeTags = [...new Set(recipe.tags.map(tag => tag.name))];
-          
-          // For each tag, add the recipe to that tag's array
-          recipeTags.forEach(tagName => {
-            if (!acc[tagName]) {
-              acc[tagName] = [];
-            }
-            acc[tagName].push(recipe);
-          });
-          
-          return acc;
-        }, {} as { [tag: string]: Recipe[] });
-  
-        console.log('Recipes by Tag:', tagMap);
-        return tagMap;
-      })
-    );
-  
     // Extract unique tag names
     this.allTags$ = this.recipes$.pipe(
-      tap(recipes => {
-        console.log('Tags:', recipes.flatMap(recipe => recipe.tags.map(tag => tag.name)));
-      }),
       map(recipes => {
-        // Flatten and get unique tag names
         const allTagNames = recipes.flatMap(recipe => 
           recipe.tags.map(tag => tag.name)
         );
         return [...new Set(allTagNames)];
       })
     );
+
+    // Create filtered recipes observable
+    this.filteredRecipes$ = combineLatest([
+      this.recipes$, 
+      this.selectedTags$
+    ]).pipe(
+      map(([recipes, selectedTags]) => {
+        // If no tags are selected, return all recipes
+        if (selectedTags.length === 0) {
+          return recipes;
+        }
+        
+        // Filter recipes that have ALL selected tags
+        return recipes.filter(recipe => 
+          selectedTags.every(tag => 
+            recipe.tags.some(recipeTag => recipeTag.name === tag)
+          )
+        );
+      })
+    );
+  }
+
+  // Method to toggle tag selection
+  toggleTag(tag: string) {
+    const currentTags = this.selectedTagsSubject.value;
+    const updatedTags = currentTags.includes(tag)
+      ? currentTags.filter(t => t !== tag)  // Remove tag if already selected
+      : [...currentTags, tag];  // Add tag if not selected
+    
+    this.selectedTagsSubject.next(updatedTags);
+  }
+
+  // Method to clear all selected tags
+  clearTags() {
+    this.selectedTagsSubject.next([]);
   }
 }
